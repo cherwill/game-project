05d937d3fcfd728fd621c64993adf9fc0c85e847
cherwil <wchering@gmail.com>
2017-02-12 23:08:45 +0000

    -started project for platformer

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+    [SerializeField]
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //player movement
+        transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0);
+    }
+}
