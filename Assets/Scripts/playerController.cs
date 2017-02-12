de608a3dd7bc47baa30ed4e56f7bc8032db12675
Imran Bepari <imranbepari97@gmail.com>
2017-02-12 23:40:18 +0000

    Added binary jump to player controller

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class playerController : MonoBehaviour {
     [SerializeField]
     public float speed;
+    Rigidbody2D rb;
+    [SerializeField] float jumpHeight;
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody2D>();
+        jumpHeight = 500f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         //player movement
         transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0);
+        
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(jumpHeight * Vector2.up);
+        }
+            
     }
 }
