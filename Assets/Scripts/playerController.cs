599fe07644afd13dacef84c3fe1beae085dc01cd
Imran Bepari <imranbepari97@gmail.com>
2017-02-15 23:01:23 +0000

    Variable jump height now works, holding jumps higher

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -6,32 +6,28 @@ public class playerController : MonoBehaviour {
     [SerializeField]
     public float speed;
     Rigidbody2D rb;
-    [SerializeField] float jumpHeight;
-    bool onGround = true;
+    bool onGround = false;
     BoxCollider2D bc2d;
-    public float jumpShortSpeed = 300f;   // Velocity for the lowest jump
-    public float jumpSpeed = 1000f;          // Velocity for the highest jump
+    public float shortJump = 3f;   // Velocity for the lowest jump
+    public float fullJump = 10f;          // Velocity for the highest jump
     bool jump = false;
     bool jumpCancel = false;
+    CapsuleCollider2D cc;
 
 
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody2D>();
         bc2d = GetComponent<BoxCollider2D>();
-        jumpHeight = 500f;
+        cc = GetComponent<CapsuleCollider2D>();
 	}
-	//test comment
-	// Update is called once per frame
-	void Update () {
+    //test comment
+    // Update is called once per frame
+    void Update() {
         //player movement
         transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0);
-        
-        //if(Input.GetButtonDown("Jump") && onGround ) {}
-        //{
-        //    rb.AddForce(jumpHeight * Vector2.up);
-        //}
 
+        //onGround = Physics.CheckCapsule(cc.bounds.center, new Vector3(cc.bounds.center.x, cc.bounds.min.y - 0.1f, cc.bounds.center.z), 0.18f);
 
         if (Input.GetButtonDown("Jump") && onGround)   // Player starts pressing the button
             jump = true;
@@ -41,18 +37,34 @@ public class playerController : MonoBehaviour {
 
     void FixedUpdate()
     {
-        // Normal jump (full speed)
+        // Normal jump
         if (jump)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+            rb.velocity = new Vector2(rb.velocity.x, fullJump);
             jump = false;
         }
         // Cancel the jump when the button is no longer pressed
         if (jumpCancel)
         {
-            if (rb.velocity.y > jumpShortSpeed)
-                rb.velocity = new Vector2(rb.velocity.x, jumpShortSpeed);
+            if (rb.velocity.y > shortJump)
+                rb.velocity = new Vector2(rb.velocity.x, shortJump);
             jumpCancel = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Terrain")
+        {
+            onGround = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Terrain")
+        {
+            onGround = false;
         }
     }
 }
